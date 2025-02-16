a2c7b1d9105967d6f80ea40837d95ba0a45a7e62
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-02-16 23:40:04 +0100

    feat: optimize object parser

--- lib/src/rules/object_rule.dart
+++ lib/src/rules/object_rule.dart
@@ -2,46 +2,56 @@ import 'package:vine/src/contracts/schema.dart';
 import 'package:vine/src/contracts/vine.dart';
 import 'package:vine/src/field_pool.dart';
 
-void objectRuleHandler(VineValidationContext ctx, FieldContext field,
-    Map<String, VineSchema> payload, String? message) {
-  if (field.value is! Map) {
+void objectRuleHandler(
+  VineValidationContext ctx,
+  FieldContext field,
+  Map<String, VineSchema> payload,
+  String? message,
+) {
+  final fieldValue = field.value;
+  if (fieldValue is! Map<String, dynamic>) {
     final error = ctx.errorReporter.format('object', field, message, {});
-    return ctx.errorReporter.report('object', field.customKeys, error);
+    ctx.errorReporter.report('object', field.customKeys, error);
+
+    return;
   }
 
-  for (final MapEntry(:key, :value) in payload.entries) {
-    final currentField = FieldPool.acquire(
-        key, (field.value as Map).containsKey(key) ? field.value[key] : MissingValue())
-      ..customKeys.addAll(field.customKeys);
+  final Map<String, dynamic> resultMap = {};
+  bool shouldBreak = false;
 
-    if (value is VineArray) {
-      field.customKeys.add(key);
+  for (final entry in payload.entries) {
+    if (shouldBreak) break;
+
+    final key = entry.key;
+    final schema = entry.value;
+
+    final currentField =
+        FieldPool.acquire(key, fieldValue.containsKey(key) ? field.value[key] : MissingValue())
+          ..customKeys.addAll(List.of(field.customKeys, growable: false));
+
+    switch (schema) {
+      case VineArray():
+        field.customKeys.add(key);
+      case VineObject():
+        if (!fieldValue.containsKey(key)) {
+          final error = ctx.errorReporter.format('object', field, message, {});
+          ctx.errorReporter.report('object', field.customKeys, error);
+        }
+        currentField.customKeys.add(key);
+        field.customKeys.add(key);
     }
 
-    if (value is VineObject) {
-      if (field.value case Map values when !values.containsKey(key)) {
-        final error = ctx.errorReporter.format('object', field, message, {});
-        ctx.errorReporter.report('object', field.customKeys, error);
-      }
+    schema.parse(ctx, currentField);
+    resultMap[key] = currentField.value;
 
-      currentField.customKeys.add(key);
-      field.customKeys.add(key);
-    }
-
-    value.parse(ctx, currentField);
-
-    field.mutate({...field.value as Map<String, dynamic>, currentField.name: currentField.value});
+    shouldBreak = !currentField.canBeContinue || ctx.errorReporter.hasError;
     FieldPool.release(currentField);
-
-    if (!currentField.canBeContinue) break;
-    if (ctx.errorReporter.hasError) break;
   }
 
-  final copy = {...field.value as Map<String, dynamic>};
-  for (final value in (field.value as Map<String, dynamic>).entries) {
-    if (!payload.containsKey(value.key)) {
-      copy.remove(value.key);
-    }
-  }
-  field.mutate(copy);
+  final cleanedMap = {
+    for (final key in fieldValue.keys)
+      if (payload.containsKey(key)) key: resultMap[key] ?? fieldValue[key]
+  };
+
+  field.mutate(cleanedMap);
 }
